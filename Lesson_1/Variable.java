1f3ee9fd17248bb22623852fb94e06bcd4755656
Svetlana Motora <motora.svetlana@gmail.com>
2020-03-02 23:26:12 +0300

    implement HW_1.1

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,20 +1,21 @@
-public class Variable{
-	public static void main(String[] args){
-		byte Win=7;
-		short Intel=7;
-		int GeForce=660;
-		long Ram=6144;
-		float Cpu=2.3f;
-		double Ind=5.9;
-		char Hdd = 'C';
-		boolean Laptop=true;
-			System.out.println("Version Windows: "+Win);
-			System.out.println("Intel Core i"+Intel);
-			System.out.println("NVIDEA GeForce GTX "+ GeForce+"M");
-			System.out.println("RAM:"+Ram+" MB");
-			System.out.println("CPU: "+Cpu+"GHz");
-			System.out.println("Windows System Assessment Tool: "+ Ind);
-			System.out.println("One of HDD: "+Hdd);
-			System.out.println("Is the computer a laptop? "+Laptop);
+public class Variable {
+	public static void main(String[] args) {
+		byte win = 7;
+		short intel = 7;
+		int geforce = 660;
+		long ram = 6144;
+		float cpu = 2.3f;
+		double ind = 5.9;
+		char hdd = 'C';
+		boolean isLaptop = true;
+
+				System.out.println("Version Windows: " + win);
+				System.out.println("Intel Core i" + intel);
+				System.out.println("NVIDEA GeForce GTX " + geforce + "M");
+				System.out.println("RAM:" + ram + " MB");
+				System.out.println("CPU: " + cpu + "GHz");
+				System.out.println("Windows System Assessment Tool: " + ind);
+				System.out.println("One of HDD: " + hdd);
+				System.out.println("Is the computer a laptop? " + isLaptop);
 	}
 }

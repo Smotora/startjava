59efa043bb244f3271af6fdca69a78a321e6e4a4
Svetlana Motora <motora.svetlana@gmail.com>
2020-03-10 22:59:29 +0300

    implement HW_3.2

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -2,43 +2,49 @@ import java.util.Scanner;
 public class Calculator {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner (System.in);
-		double a;
-		System.out.println ("Введите первое число: ");
-		while (!scan.hasNextInt()) {
-			System.out.println ("Введеные данные не являются числом. Повторите ввод первого числа: ");
+		System.out.println("Введите первое число: ");
+		
+		while (!scan.hasNextDouble()) {
+			System.out.println("Введеные данные не являются числом. Повторите ввод первого числа: ");
 			scan.next();
 		}
-		a = scan.nextDouble();
+		double a= scan.nextDouble();
 	
-		double b;
-		System.out.println ("Введите второе число: ");
-		while (!scan.hasNextInt()) {
-			System.out.println ("Введеные данные не являются числом. Повторите ввод второго числа: ");
-			scan.next();
-		}
-		b = scan.nextDouble();
-
-		System.out.println ("Введите операцию: +, -, *, /, ^, % ");
+		System.out.println("Введите операцию: +, -, *, /, ^, % ");
+		
 		while (!scan.hasNext("[-+*/^%]")) {
-			System.out.println ("Введеные данные не являются арифметической операцией. Повторите ввод операции: ");
+			System.out.println("Введеные данные не являются арифметической операцией. Повторите ввод операции: ");
 			scan.next();
 		}
-		char oper = scan.next().charAt(0);
-
-		if (oper == '+') {
-			System.out.println ( "Сумма введённых чисел: " + (a + b));
-		} else if (oper == '-') {
-			System.out.println ( "Разность введённых чисел: " + (a - b));
-		} else if (oper == '*') {
-			System.out.println ( "Произведение введённых чисел: " + (a * b));
-		} else if (oper == '/' & b==0) {
-			System.out.println ("Деление на 0 запрещено!");
-		} else if (oper == '/') {
-			System.out.println ("Частное введённых чисел: " + (a / b));
-		} else if (oper == '^') {
-			System.out.println ("Первое число в степени второго числа: " + Math.pow(a, b));
+		char operator = scan.next().charAt(0);
+		
+		System.out.println("Введите второе число: ");
+		while (!scan.hasNextDouble()) {
+			System.out.println("Введеные данные не являются числом. Повторите ввод второго числа: ");
+			scan.next();
+		}
+		double b= scan.nextDouble();
+		
+		if (operator == '+') {
+			System.out.println( "Сумма введённых чисел: " + (a + b));
+		} else if (operator == '-') {
+			System.out.println( "Разность введённых чисел: " + (a - b));
+		} else if (operator == '*') {
+			System.out.println( "Произведение введённых чисел: " + (a * b));
+		} else if (operator == '/' & b == 0) {
+			System.out.println("Деление на 0 запрещено!");
+		} else if (operator == '/') {
+			System.out.println("Частное введённых чисел: " + (a / b));
+		} else if (operator == '^') {
+			double result = 1.0;
+			double i = 1.0;
+			while (i <= b) {
+				result = a * result;
+				i++;
+			}
+			System.out.println("Первое число в степени второго числа: " + result);
 		} else {
-			System.out.println ("Остаток от деления первого числа на второе: " + (a % b));
+			System.out.println("Остаток от деления первого числа на второе: " + (a % b));
 		}
 	}
 }

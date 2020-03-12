03581480845c3858ac315f549b391a86bf4dfc1c
Svetlana Motora <motora.svetlana@gmail.com>
2020-03-12 22:49:07 +0300

    implement HW_5.1

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,21 +1,20 @@
 import java.util.Scanner;
+
 public class MyFirstGame {
 	public static void main(String[] args) {
 		System.out.println("Попробуйте угадать целое число, загаданное в диапазоне (0; 100]: ");
-		int numberComp = 1 + (int) (Math.random() * 101);
+		int hiddenNumber = (int) (Math.random() * 101);
 		Scanner scanNum = new Scanner (System.in);
-		int numberUser = scanNum.nextInt();
+		int userNumber = scanNum.nextInt();
+
 		do {
-			if (numberUser>numberComp){
-			System.out.println("Ввёденное Вами число больше того, что загадано, продолжайте: ");
-			numberUser = scanNum.nextInt();
+			if (userNumber>hiddenNumber) {
+				System.out.println("Ввёденное Вами число больше того, что загадано, продолжайте: ");
+				userNumber = scanNum.nextInt();
 			} else {
-			System.out.println("Ввёденное Вами число меньше того, что загадано, продолжайте: ");
-			numberUser = scanNum.nextInt();
+				System.out.println("Ввёденное Вами число меньше того, что загадано, продолжайте: ");
+				userNumber = scanNum.nextInt();
 			}
-		} while (numberComp!= numberUser);
-		if (numberComp == numberUser){
-			System.out.println("Вы угадали!");
-		}
+		} while (hiddenNumber!= userNumber);
 	}
 }

5ed46a6f911103642243d86a78e57af37a214afd
Svetlana Motora <motora.svetlana@gmail.com>
2020-03-14 23:34:11 +0300

    implement HW_5.2

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -4,17 +4,17 @@ public class MyFirstGame {
 	public static void main(String[] args) {
 		System.out.println("Попробуйте угадать целое число, загаданное в диапазоне (0; 100]: ");
 		int hiddenNumber = (int) (Math.random() * 101);
-		Scanner scanNum = new Scanner (System.in);
-		int userNumber = scanNum.nextInt();
+		Scanner scan = new Scanner (System.in);
+		int userNumber = scan.nextInt();
 
 		do {
-			if (userNumber>hiddenNumber) {
+			if (userNumber > hiddenNumber) {
 				System.out.println("Ввёденное Вами число больше того, что загадано, продолжайте: ");
-				userNumber = scanNum.nextInt();
-			} else {
+				userNumber = scan.nextInt();
+			} else if (userNumber < hiddenNumber) {
 				System.out.println("Ввёденное Вами число меньше того, что загадано, продолжайте: ");
-				userNumber = scanNum.nextInt();
+				userNumber = scan.nextInt();
 			}
-		} while (hiddenNumber!= userNumber);
+		} while (hiddenNumber != userNumber);
 	}
 }

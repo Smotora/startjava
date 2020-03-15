579f62c13c1df0d6981ec43eba607d131d0a8d72
Svetlana Motora <motora.svetlana@gmail.com>
2020-03-15 23:36:37 +0300

    implement HW_4.1

--- Lesson_1/Unicode.java
+++ Lesson_1/Unicode.java
@@ -1,8 +1,8 @@
 public class Unicode {
 	public static void main(String[] args) {
 		System.out.println("Символы UNICODE в диапазоне [9398, 10178]: " );
-		for (int symbol = 9398; symbol <= 10178; symbol++) {
-			System.out.println("Символ № " + symbol +": "+ (char) symbol);
+		for (int i = 9398; i <= 10178; i++) {
+			System.out.println("Символ № " + i +": "+ (char) i);
 		}
 	}
 }

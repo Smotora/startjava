3ff783f40ad5033baf15151644038d0eeb46e25d
Svetlana Motora <motora.svetlana@gmail.com>
2020-03-16 22:28:19 +0300

    implement HW_6.1

--- Lesson_1/Cycle.java
+++ Lesson_1/Cycle.java
@@ -1,12 +1,12 @@
 public class Cycle {
 	public static void main(String[] args) {
-		System.out.println("Числа на отрезке от 0 до 20: " );
+		System.out.println("Числа на отрезке от 0 до 20: ");
 		for (int i = 0; i <= 20; i++) {
 			System.out.println(i);
 		}
 
 		int j = 6;
-		System.out.println("Числа на отрезке от 6 до -6 с шагом 2: " );
+		System.out.println("Числа на отрезке от 6 до -6 с шагом 2: ");
 		while (j >= -6) {
 			System.out.println(j);
 			j += -2;
@@ -14,12 +14,10 @@ public class Cycle {
 
 		int k = 10;
 		int sum = 0;
-		System.out.println("Сумма нечётных чисел на отрезке от 10 до 20: " );
+		System.out.println("Сумма нечётных чисел на отрезке от 10 до 20: ");
 		do {
 			if (k % 2 != 0) {
 				sum += k;
-			} else {
-				sum += 0;
 			}
 			k++;
 		} while (k <= 20);

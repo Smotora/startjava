7323dd45ffa7af681838eefd4e265c9f97152e50
Svetlana Motora <motora.svetlana@gmail.com>
2020-03-19 23:21:40 +0300

    implement HW_3

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,33 +1,85 @@
 public class Wolf {
-    char sex;
-    String name;
-    float weight;
-    int age;
-    String color;
+    private char sex;
+    private String name;
+    private float weight;
+    private int age;
+    private String color;
 
-    int walk() {
+    public char getSex() {
+        return sex;
+    }
+    public void setSex(char sex) {
+        if (sex != 'M' & sex != 'F') {
+            System.out.println("Некорректный пол");
+        } else {
+            this.sex = sex;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        if (name == "") {
+            System.out.println("Не введена кличка");
+        } else {
+            this.name = name;
+        }
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+    public void setWeight(float weight) {
+        if (weight <= 0) {
+            System.out.println("Некорректный вес");
+        } else {
+            this.weight = weight;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        if (color == "") {
+            System.out.println("Не введен цвет");
+        } else {
+            this.color = color;
+        }
+    }
+
+    public int walk() {
         int walkDistance = 80;
         return walkDistance;
     }
 
-    void sit() {
+    public void sit() {
         System.out.println("Сидит");
     }
 
-    int run() {
+    public int run() {
         int runDistance = 20;
         return runDistance;
     }
 
-    String howl() {
+    public String howl() {
         return "У-у-у";
     }
 
-    boolean hunt() {
+    public boolean hunt() {
         System.out.println("Добывает пищу охотой");
         return true;
     }
 }
-
-
-

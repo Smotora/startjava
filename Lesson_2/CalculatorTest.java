9945e9a900390ad4f413d902f32e99856615e401
Svetlana Motora <motora.svetlana@gmail.com>
2020-03-28 19:51:44 +0300

    implement HW_4.2

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -2,33 +2,32 @@ import java.util.Scanner;
 
 public class CalculatorTest {
 	public static void main(String[] args) {
-		String playAgain = null;
+		String againCalc = null;
 		Scanner scan = new Scanner(System.in);
 		do {
-		Calculator calc = new Calculator();
-		
-		System.out.println("Введите первое число: ");
-		calc.getA();
-		
-		System.out.println("Введите операцию: ");
-		calc.getOperator();
-
-		System.out.println("Введите второе число:  ");
-		calc.getB();
-		
-		System.out.println("Результат: " + calc.calculate());
-		System.out.println("Хотите продолжить? [да/нет]: ");
-		playAgain = scan.next();
-		} while (playAgain.equals("да"));
-		
-		while (!playAgain.equals("да")) {
-			if (playAgain.equals("нет")) {
+			Calculator calc = new Calculator();
+			
+			System.out.println("Введите первое число: ");
+			calc.getA();
+			
+			System.out.println("Введите операцию: ");
+			calc.getOperator();
+			
+			System.out.println("Введите второе число:  ");
+			calc.getB();
+			
+			System.out.println("Результат: " + calc.calculate());
+			System.out.println("Хотите продолжить? [да/нет]: ");
+			againCalc = scan.next();
+			do {
+				if (againCalc.equals("нет")) {
 				System.out.println("До встречи!");
 				break;
 			} else {
-			System.out.println("Введены неверные данные. Хотите продолжить? [да/нет]: ");
-			playAgain = scan.next();
+				System.out.println("Введены неверные данные. Хотите продолжить? [да/нет]: ");
+				againCalc = scan.next();
 			}
-		}
+			} while (!againCalc.equals("да"));
+		} while (againCalc.equals("да"));
 	}
 }

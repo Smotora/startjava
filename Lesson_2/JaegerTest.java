4bc76fd97bb92ef50985d4a95f5eb99b9c2ded1a
Svetlana Motora <motora.svetlana@gmail.com>
2020-04-04 21:29:03 +0300

    implement HW_5.1

--- Lesson_2/JaegerTest.java
+++ Lesson_2/JaegerTest.java
@@ -1,58 +1,26 @@
 public class JaegerTest {
 	public static void main(String[] args) {
-		Jaeger jaegerOne = new Jaeger();
-		jaegerOne.setModelName ("Saber Athena");
-		System.out.println("Model Name Jaeger One: " + jaegerOne.getModelName());
-
-		jaegerOne.setMark("Mark - VII");
-		System.out.println("Mark Jaeger One: " + jaegerOne.getMark());
-
-		jaegerOne.setOrigin("China");
-		System.out.println("Origin Jaeger One: " + jaegerOne.getOrigin());
-
-		jaegerOne.setHeight(252f);
-		System.out.println("Height Jaeger One in ft: " + jaegerOne.getHeight());
-
-		jaegerOne.setWeight(1.638f);
-		System.out.println("Weight Jaeger One in tons: " + jaegerOne.getWeight());
-
-		jaegerOne.setSpeed(11);
-		System.out.println("Speed Jaeger One: " + jaegerOne.getSpeed());
-
-		jaegerOne.setStrength(8);
-		System.out.println("Strength Jaeger One: " + jaegerOne.getStrength());
-
-		jaegerOne.setArmor(5);
-		System.out.println("Armor Jaeger One: " + jaegerOne.getArmor());
-
+		Jaeger jaegerOne = new Jaeger("Saber Athena", "Mark - VII", "China", 252f, 1.638f, 11, 8, 5);
+		System.out.println("Model Name Jaeger One: " + jaegerOne.modelName);
+		System.out.println("Mark Jaeger One: " + jaegerOne.mark);
+		System.out.println("Origin Jaeger One: " + jaegerOne.origin);
+		System.out.println("Height Jaeger One in ft: " + jaegerOne.height);
+		System.out.println("Weight Jaeger One in tons: " + jaegerOne.weight);
+		System.out.println("Speed Jaeger One: " + jaegerOne.speed);
+		System.out.println("Strength Jaeger One: " + jaegerOne.strength);
+		System.out.println("Armor Jaeger One: " + jaegerOne.armor);
 		System.out.println(jaegerOne.pilot());
 		jaegerOne.move();
 
-		Jaeger jaegerTwo = new Jaeger();
-		jaegerTwo.setModelName("November Ajax");
-		System.out.println("Model Name Jaeger Two: " + jaegerTwo.getModelName());
-
-		jaegerTwo.setMark("Mark - VI");
-		System.out.println("Mark Jaeger Two: " + jaegerTwo.getMark());
-
-		jaegerTwo.setOrigin("PPDC");
-		System.out.println("Origin Jaeger Two: " + jaegerTwo.getOrigin());
-
-		jaegerTwo.setHeight(259f);
-		System.out.println("Height Jaeger Two in ft: " + jaegerTwo.getHeight());
-
-		jaegerTwo.setWeight(2.055f);
-		System.out.println("Weight Jaeger Two in tons: " + jaegerTwo.getWeight());
-
-		jaegerTwo.setSpeed(4);
-		System.out.println("Speed Jaeger Two: " + jaegerTwo.getSpeed());
-
-		jaegerTwo.setStrength(5);
-		System.out.println("Strength Jaeger Two: " + jaegerTwo.getStrength());
-
-		jaegerTwo.setArmor(4);
-		System.out.println("Armor Jaeger Two: " + jaegerTwo.getArmor());
-
+		Jaeger jaegerTwo = new Jaeger("November Ajax", "Mark - VI", "PPDC", 259f, 2.055f, 4, 5, 4);
+		System.out.println("Model Name Jaeger Two: " + jaegerTwo.modelName);
+		System.out.println("Mark Jaeger Two: " + jaegerTwo.mark);
+		System.out.println("Origin Jaeger Two: " + jaegerTwo.origin);
+		System.out.println("Height Jaeger Two in ft: " + jaegerTwo.height);
+		System.out.println("Weight Jaeger Two in tons: " + jaegerTwo.weight);
+		System.out.println("Speed Jaeger Two: " + jaegerTwo.speed);
+		System.out.println("Strength Jaeger Two: " + jaegerTwo.strength);
+		System.out.println("Armor Jaeger Two: " + jaegerTwo.armor);
 		jaegerTwo.useWeapon();
 		System.out.println(jaegerTwo.attackKaiju());
 	}

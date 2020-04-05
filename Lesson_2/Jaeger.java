c65908bf86c96b25db333bb1493c1159b388044b
Svetlana Motora <motora.svetlana@gmail.com>
2020-04-05 23:34:54 +0300

    implement HW_5.1

--- Lesson_2/Jaeger.java
+++ Lesson_2/Jaeger.java
@@ -1,22 +1,54 @@
 public class Jaeger {
-	String modelName;
-	String mark;
-	String origin;
-	float height;
-	float weight;
-	int speed;
-	int strength;
-	int armor;
+	private String modelName;
+	private String mark;
+	private String origin;
+	private float height;
+	private float weight;
+	private int speed;
+	private int strength;
+	private int armor;
 
-	public Jaeger(String modelName, String mark, String origin, float height, float weight,int speed, int strength, int armor) {
-		this.modelName = modelName;
+	Jaeger (String modelName, String mark, String origin, float height, float weight,int speed, int strength, int armor) {
+		this.modelName= modelName;
 		this.mark = mark;
-		this.origin = origin;
+		this.origin= origin;
 		this.height = height;
 		this.weight = weight;
 		this.speed = speed;
 		this.strength = strength;
 		this.armor = armor;
+	}
+	
+	public String getModelName() {
+		return modelName;
+	}
+	
+	public String getMark() {
+		return mark;
+	}
+	
+	public String getOrigin() {
+		return origin;
+	}
+	
+	public float getHeight() {
+		return height;
+	}
+	
+	public float getWeight() {
+		return weight;
+	}
+	
+	public int getSpeed() {
+		return speed;
+	}
+	
+	public int getStrength() {
+		return strength;
+	}
+	
+	public int getArmor() {
+		return armor;
 	}
 
 	public boolean pilot() {

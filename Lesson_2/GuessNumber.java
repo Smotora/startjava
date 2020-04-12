591673be5f276cfeae657a44d90ae5fd6a2a3942
Svetlana Motora <motora.svetlana@gmail.com>
2020-04-12 20:01:16 +0300

    implement HW_6.2

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -2,41 +2,38 @@ import java.util.Scanner;
 
 public class GuessNumber {
 	Scanner scan = new Scanner (System.in);
-	Player playerOne;
-	Player playerTwo;
+	private Player playerOne;
+	private Player playerTwo;
 	int hiddenNumber = (int) (Math.random() * 101);
 	
-    public GuessNumber(Player playerOne, Player playerTwo) {
-        this.playerOne = playerOne;
-        this.playerTwo = playerTwo;
-    }
-    
-	public void newGame() {
+	public GuessNumber(Player playerOne, Player playerTwo) {
+		this.playerOne = playerOne;
+		this.playerTwo = playerTwo;
+	}
+
+	public void playGame() {
 		System.out.println("Попробуйте угадать целое число, загаданное в диапазоне (0; 100]: ");
 		while(true) {
 			System.out.println(playerOne.getName() + ", Ваш ход. Введите число: ");
-			playerOne.setPlayerNumber(scan.nextInt());
-			playerOne.getPlayerNumber();
-			if (playerOne.getPlayerNumber() > hiddenNumber) {
+			playerOne.setNumber(scan.nextInt());
+			if (playerOne.getNumber() > hiddenNumber) {
 				System.out.println("Ввёденное Вами число больше того, что загадано.");
-			} else if (playerOne.getPlayerNumber() < hiddenNumber) {
-				System.out.println("Ввёденное Вами число меньше того, что загадано. ");
-			} else if (playerOne.getPlayerNumber() == hiddenNumber) {
+			} else if (playerOne.getNumber() < hiddenNumber) {
+				System.out.println("Ввёденное Вами число меньше того, что загадано.");
+			} else if (playerOne.getNumber() == hiddenNumber) {
 				System.out.println(playerOne.getName() + " победил! Загаданное число: " + hiddenNumber);
 				break;
 			}
 			System.out.println(playerTwo.getName() + ", Ваш ход. Введите число: ");
-			playerTwo.setPlayerNumber(scan.nextInt());
-			playerTwo.getPlayerNumber();
-			if (playerTwo.getPlayerNumber() > hiddenNumber) {
+			playerTwo.setNumber(scan.nextInt());
+			if (playerTwo.getNumber() > hiddenNumber) {
 				System.out.println("Ввёденное Вами число больше того, что загадано.");
-			} else if (playerTwo.getPlayerNumber() < hiddenNumber) {
-				System.out.println("Ввёденное Вами число меньше того, что загадано. ");
-			} else if (playerTwo.getPlayerNumber() == hiddenNumber) {
+			} else if (playerTwo.getNumber() < hiddenNumber) {
+				System.out.println("Ввёденное Вами число меньше того, что загадано.");
+			} else if (playerTwo.getNumber() == hiddenNumber) {
 				System.out.println(playerTwo.getName() + " победил! Загаданное число: " + hiddenNumber);
 				break;
 			}
-		} 
-		
+		}
 	}
 }

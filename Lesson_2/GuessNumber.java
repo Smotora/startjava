5d788e635b1b23f4ba4b055fb9519920520f215b
Svetlana Motora <motora.svetlana@gmail.com>
2020-04-12 16:19:42 +0300

    implement HW_6.1

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -1,55 +1,42 @@
 import java.util.Scanner;
 
 public class GuessNumber {
+	Scanner scan = new Scanner (System.in);
+	Player playerOne;
+	Player playerTwo;
+	int hiddenNumber = (int) (Math.random() * 101);
+	
+    public GuessNumber(Player playerOne, Player playerTwo) {
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+    }
+    
 	public void newGame() {
-		Player playerOne = new Player();
-		Player playerTwo = new Player();
-		int hiddenNumber = 0;
-		String playerOneName = null;
-		String playerTwoName = null;
-		Scanner scan = new Scanner (System.in);
-		int playerOneNumber = 0;
-		int playerTwoNumber = 0;
-		String againGame = null;
-
-		do {
-			System.out.println("Попробуйте угадать целое число, загаданное в диапазоне (0; 100]: ");
-			hiddenNumber = (int) (Math.random() * 101);
-			System.out.println("Введите имя первого игрока, затем имя второго игрока.");
-			playerOneName = playerOne.getName();
-			playerTwoName = playerTwo.getName();
-			do {
-				System.out.println(playerOneName + ", Ваш ход: ");
-				playerOneNumber = playerOne.getPlayerNumber();
-				if (playerOneNumber > hiddenNumber) {
-					System.out.println("Ввёденное Вами число больше того, что загадано, ход игрока " + playerTwoName);
-				} else if (playerOneNumber < hiddenNumber) {
-					System.out.println("Ввёденное Вами число меньше того, что загадано, ход игрока " + playerTwoName);
-				} else {
-					break;
-				}
-				System.out.println(playerTwoName + ", Ваш ход: ");
-				playerTwoNumber = playerTwo.getPlayerNumber();
-				if (playerTwoNumber > hiddenNumber) {
-					System.out.println("Ввёденное Вами число больше того, что загадано, ход игрока " + playerOneName);
-				} else if (playerTwoNumber < hiddenNumber) {
-					System.out.println("Ввёденное Вами число меньше того, что загадано, ход игрока " + playerOneName);
-				} else {
-					break;
-				}
-			} while (hiddenNumber != playerOneNumber && hiddenNumber != playerTwoNumber);
-				if (playerOneNumber == hiddenNumber) {
-					System.out.println(playerOneName + " победил! Загаданное число: " + hiddenNumber);
-				} else if (playerTwoNumber == hiddenNumber) {
-					System.out.println(playerTwoName + " победил! Загаданное число: " + hiddenNumber);
-				}
-			System.out.println("Хотите продолжить? [yes/no]: ");
-			againGame = scan.next();
-			while (!againGame.equals("no") && !againGame.equals("yes")) {
-				System.out.println("Введены неверные данные. Хотите продолжить? [yes/no]: ");
-				againGame = scan.next();
+		System.out.println("Попробуйте угадать целое число, загаданное в диапазоне (0; 100]: ");
+		while(true) {
+			System.out.println(playerOne.getName() + ", Ваш ход. Введите число: ");
+			playerOne.setPlayerNumber(scan.nextInt());
+			playerOne.getPlayerNumber();
+			if (playerOne.getPlayerNumber() > hiddenNumber) {
+				System.out.println("Ввёденное Вами число больше того, что загадано.");
+			} else if (playerOne.getPlayerNumber() < hiddenNumber) {
+				System.out.println("Ввёденное Вами число меньше того, что загадано. ");
+			} else if (playerOne.getPlayerNumber() == hiddenNumber) {
+				System.out.println(playerOne.getName() + " победил! Загаданное число: " + hiddenNumber);
+				break;
 			}
-		} while (againGame.equals("yes"));
-		System.out.println("Игра окончена!");
+			System.out.println(playerTwo.getName() + ", Ваш ход. Введите число: ");
+			playerTwo.setPlayerNumber(scan.nextInt());
+			playerTwo.getPlayerNumber();
+			if (playerTwo.getPlayerNumber() > hiddenNumber) {
+				System.out.println("Ввёденное Вами число больше того, что загадано.");
+			} else if (playerTwo.getPlayerNumber() < hiddenNumber) {
+				System.out.println("Ввёденное Вами число меньше того, что загадано. ");
+			} else if (playerTwo.getPlayerNumber() == hiddenNumber) {
+				System.out.println(playerTwo.getName() + " победил! Загаданное число: " + hiddenNumber);
+				break;
+			}
+		} 
+		
 	}
 }

8a0e600bafe79272a62fa790cb148a868a8ed7df
Svetlana Motora <motora.svetlana@gmail.com>
2020-05-14 23:30:58 +0300

    add lesson_5

---
+++ src/com/startjava/lesson_5/init_db.sql
@@ -0,0 +1,12 @@
+INSERT INTO Jaegers(modelName, mark, height, weight, status, origin, launch, kaijuKill)
+VALUES 
+('Saber Athena', '7', '252', '1638', 'N', 'China', '2033-12-20', '2'),
+('Midnight Winter', '5', '250', '2325', 'Y', 'Soviet Union', '2033-06-02', '2'),
+('Azure Defiant', '3', '245', '1579', 'N', 'Phillipines', '2019-01-06', '9'),
+('Trider Zeta', '5', '278', '2010', 'Y', 'United States', '2029.12.28', '9'),
+('Ace Blitzkrieg', '6', '256', '1946', 'Y', 'Germany', '2007.01.01', '3'),
+('Barbatos Onslaught', '6', '240', '1978', 'Y', 'Singapore', '2035.08.31', '0'),
+('Bracer Phoenix', '5', '232', '2855', 'N', 'China', '2025.11.01', '1'),
+('Horus Seravee', '7', '220', '2320', 'Y', 'Singapore ', '2036.04.13', '1'),
+('Ranger Sierra', '7', '325', '34350', 'N', 'United States', '2031.01.17', '4'),
+('Noveria Alpha', '4', '260', '1300', 'Y', 'Russia', '2018.09.09', '18');
